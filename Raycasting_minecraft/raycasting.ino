51d538d013ce0caec82ce31bfad50270e6f3b6dc
方布块 <100364063+zjsss900@users.noreply.github.com>
2023-02-13 21:00:02 +0800

    Update raycasting.ino

--- Raycasting_minecraft/raycasting.ino
+++ Raycasting_minecraft/raycasting.ino
@@ -1,15 +1,15 @@
 void RAYCASTING() {
   for (int x = 0; x < screenWidth; x++) {
-    double cameraX = 2 * x / double(screenWidth) - 1;
-    double rayDirX = dirX + planeX * cameraX;
-    double rayDirY = dirY + planeY * cameraX;
+    float cameraX = 2 * x / double(screenWidth) - 1;
+    float rayDirX = dirX + planeX * cameraX;
+    float rayDirY = dirY + planeY * cameraX;
     int mapX = int(posX);
     int mapY = int(posY);
-    double sideDistX;
-    double sideDistY;
-    double deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
-    double deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
-    double perpWallDist;
+    float sideDistX;
+    float sideDistY;
+    float deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
+    float deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
+    float perpWallDist;
     int stepX, stepY;
     int hit = 0;
     int side;
@@ -67,7 +67,7 @@ void RAYCASTING() {
       texNum = worldMap[mapX][mapY] - 1;
       DRAW_TEX(texNum);
     }
-    double wallX;
+    float wallX;
     if (side == 0) {
       wallX = posY + perpWallDist * rayDirY;
     } else {
@@ -79,8 +79,8 @@ void RAYCASTING() {
 
       texX = texWidth - texX - 1;
     }
-    double oneStep = 1.0 * texHeight / lineHeight;
-    double texPos = (drawStart - pitch - screenHeight / 2 + lineHeight / 2) * oneStep;
+    float oneStep = 1.0 * texHeight / lineHeight;
+    float texPos = (drawStart - pitch - screenHeight / 2 + lineHeight / 2) * oneStep;
     for (int y = drawStart; y < drawEnd; y++) {
       int texY = (int)texPos & (texHeight - 1);
       texPos += oneStep;
@@ -93,10 +93,10 @@ void RAYCASTING() {
   }
   oldFrame = nowFrame;
   nowFrame = millis();
-  double frameTime = (nowFrame - oldFrame) / 1000.0;
+  float frameTime = (nowFrame - oldFrame) / 1000.0;
   fps = (1.0 / frameTime);
-  double moveSpeed = frameTime * 4.0;
-  double rotSpeed = frameTime * 2.0;
+  float moveSpeed = frameTime * 4.0;
+  float rotSpeed = frameTime * 2.0;
   int8_t a = down_up();
   if (a != 0 && a != 5) {
     if (millis() >= t2 + 300) {
@@ -150,7 +150,7 @@ void RAYCASTING() {
         break;
       }
     case blockdown: {
-        double oldDirX = dirX;
+        float oldDirX = dirX;
         dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
         dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
         double oldPlaneX = planeX;
@@ -159,7 +159,7 @@ void RAYCASTING() {
         break;
       }
     case blockup: {
-        double oldDirX = dirX;
+        float oldDirX = dirX;
         dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
         dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
         double oldPlaneX = planeX;
@@ -171,5 +171,69 @@ void RAYCASTING() {
         //   add_liewenTex
         break;
       }
+  }
+}
+
+void DRAW_FLOOR_AND_CEILING(){
+  uint16_t color;
+  int n=0,c=0;
+  int8_t floorTexture=3;
+  int8_t ceilingTexture=6;    
+  if(texcsh==0){ 
+    for(uint8_t a=0;a<texWidth;a++){
+      for(uint8_t b=0;b<texHeight;b++){
+        floorUsedTexture[n]=texture[floorTexture][c]<<8|texture[floorTexture][c+1];
+        ceilingUsedTexture[n]=texture[ceilingTexture][c]<<8|texture[ceilingTexture][c+1];
+        c+=2;
+        n++;
+      }
+    }
+    texcsh=1;  
+  }
+  for(int y=0;y<screenHeight;y++){
+    float rayDirX0=dirX-planeX;
+    float rayDirY0=dirY-planeY;
+    float rayDirX1=dirX+planeX;
+    float rayDirY1=dirY+planeY;
+    int p=y-(screenHeight/2+pitch);
+    float posZ=0.5*(screenHeight);
+    float rowDistance=posZ/p;
+    float floorStepX=rowDistance*(rayDirX1-rayDirX0)/screenWidth;
+    float floorStepY=rowDistance*(rayDirY1-rayDirY0)/screenWidth;
+    float floorX=posX+rowDistance*rayDirX0;
+    float floorY=posY+rowDistance*rayDirY0;    
+    for(int x=0;x<screenWidth;++x){
+      int cellX=(int)(floorX);
+      int cellY=(int)(floorY);
+      int tx=(int)(texWidth*(floorX-cellX))&(texWidth-1);
+      int ty=(int)(texHeight*(floorY-cellY))&(texHeight-1);
+      floorX+=floorStepX;
+      floorY+=floorStepY;
+      if(y > screenHeight / 2 + pitch){
+        color=floorUsedTexture[texWidth * ty + tx];      
+      }else{    
+        color=ceilingUsedTexture[texWidth*ty+tx];   
+      }
+      color=CHANGE_SIDE_TEX_COLOR(color);   
+      frameImage[x][y]=color;
+    }
+  }
+}
+
+void LOOK_UP_DOWN(){
+  if(millis()>=t6+50){
+    t6=millis();
+    if(pitch<40 && pitchBuffer==0){
+      pitch++;
+    }
+    if(pitch==40){
+      pitchBuffer=1;
+    }
+    if(pitch>-40 && pitchBuffer==1){
+      pitch--;
+    }
+    if(pitch==-40){
+      pitchBuffer=0;
+    }
   }
 }
